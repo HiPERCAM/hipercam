ecbe89aa383f2de53f8401cf2946df65888e675d
Tom Marsh <t.r.marsh@warwick.ac.uk>
2016-04-25 17:32:29 +0100

    core classes and functions and matplotlib-based plotting functions
    core.py contains some basic stuff to be used in multiple routines. mpl.py contains matplotlib-based plotting functions.

---
+++ hipercam/core.py
@@ -0,0 +1,20 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Core classes and functions for the hipercam package
+"""
+
+# Standard pre-amble from astropy
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from astropy.extern import six
+
+
+class HipercamError (Exception):
+    """
+    Error class for the hipercam package
+    """
+
+#if __name__ == '__main__':
+#    import doctest
+#    doctest.testmod()
+
---
+++ hipercam/mpl.py
@@ -0,0 +1,65 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+matplotlib plotting functions.
+"""
+
+# Standard pre-amble from astropy
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from astropy.extern import six
+from .window import *
+
+def pwin(axes, win, col='k', lw=1, **kwargs):
+    """
+    Plots boundary of a :class:`Window` as a line. (matplotlib)
+    Arguments::
+
+      axes : (:class:`matplotlib.axes.Axes`)
+           the Axes to plot to.
+
+      win : (Window)
+           the :class:`Window` to plot
+
+      col : (matplotlib colour)
+           the colour to use
+
+      lw : (float)
+           linewidth in points
+
+      kwargs : (keyword arguments)
+           other arguments to feed to :func:`matplotlib.pyplot.plot`
+    """
+    left,right,bottom,top = win.extent()
+    axes.plot([left,right,right,left,left],[bottom,bottom,top,top,bottom],c=col,lw=lw,**kwargs)
+
+def pwind(axes, wind, col='k', lw=1, aspect='equal', **kwargs):
+    """
+    Plots :class:`Windata` as an image with a line border. (matplotlib). Note that
+    the keyword arguments are only passed to :func:`imshow` and you should plot the
+    border separately if you want anything out of the ordinary.
+
+    Arguments::
+
+      axes : (:class:`matplotlib.axes.Axes`)
+           the Axes to plot to.
+
+      wind : (Windata)
+           the :class:`Windata` to plot
+
+      cmap : (colourmap from matplotlib.cm)
+           a colourmap, e.g. gray
+
+      col : (matplotlib colour)
+           the colour to use for the border
+
+      lw : (float)
+           linewidth of the border in points (0 to skip)
+
+      kwargs : (keyword arguments)
+           other arguments to feed to :func:`matplotlib.pyplot.imshow`. Useful ones are 'vmin', 'vmax', 'cmap'
+    """
+    left,right,bottom,top = wind.extent()
+
+    axes.imshow(wind.data,extent=(left,right,bottom,top),aspect=aspect,origin='lower',interpolation='nearest',**kwargs)
+
+    pwin(axes, wind, col, lw)
