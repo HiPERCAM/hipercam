b6d4684d912f4053a4e8342df537d74442ec1b68
Tom Marsh <t.r.marsh@warwick.ac.uk>
2016-04-25 17:35:25 +0100

    slightly changed the imports
    Added mpl import (sub-package level)

--- hipercam/__init__.py
+++ hipercam/__init__.py
@@ -13,5 +13,7 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from Window import *
+    from .window import *
+    import mpl
+
 
