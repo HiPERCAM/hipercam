c17343e8ad804f1c8f6060c0f152f00ce8fd09d6
Tom Marsh <t.r.marsh@warwick.ac.uk>
2016-07-01 04:46:29 +0100

    added code to plot a CCD
    plots all windows of a CCD

--- hipercam/mpl.py
+++ hipercam/mpl.py
@@ -7,7 +7,10 @@ matplotlib plotting functions.
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from astropy.extern import six
+
+from .core import *
 from .window import *
+from .ccd import *
 
 def pwin(axes, win, col='k', lw=1, **kwargs):
     """
@@ -70,3 +73,91 @@ def pwind(axes, wind, col='k', lw=1, aspect='equal', **kwargs):
                     aspect=aspect,origin='lower',cmap='Greys',
                     interpolation='nearest',**kwargs)
     pwin(axes, wind, col, lw)
+
+def pccd(axes, ccd, iset='p', plo=5., phi=95., dlo=0., dhi=1000.,
+         label=True, col='k', lw=1, aspect='equal', **kwargs):
+    """Plots :class:`CCD` as a set of :class:`Windata` objects correctly
+    positioned with respect to each other. The keyword arguments
+    are passed to :func:`imshow`. If no colour map
+    ('cmap') is specified, it will be set to 'Greys'
+
+    Arguments::
+
+      axes : (:class:`matplotlib.axes.Axes`)
+           the Axes to plot to.
+
+      ccd : (CCD)
+           the :class:`CCD` to plot
+
+      iset : (string)
+           how to set the intensity scale to be used. 'p' for percentiles
+           (set using plo and phi); 'r' for min/max range; 'd' for direct
+           value set using dlo and dhi.
+
+      plo : (float)
+           lower percentile limit to use (if iset='p')
+
+      phi : (float)
+           upper percentile limit to use (if iset='p')
+
+      dlo : (float)
+           value to use for lower intensity limit (if iset='d')
+
+      dhi : (float)
+           value to use for upper intensity limit (if iset='d')
+
+      label : (boolean)
+           whether to attach a label to the Windows or not.
+
+      col : (matplotlib colour)
+           the colour to use for the borders
+
+      lw : (float)
+           linewidth of the borders in points (0 to skip)
+
+      kwargs : (keyword arguments)
+           other arguments to feed to :func:`matplotlib.pyplot.imshow`.
+           'cmap' is useful. 'vmin' and 'vmax' are overridden by the
+           iset method.
+
+    Returns: (vmin,vmax) the intensity limits used.
+    """
+    if iset == 'p':
+        # Set intensities from percentiles
+        arrs = []
+        for wind in ccd.values():
+            arrs.append(wind.data.flatten())
+        arr = np.concatenate(arrs)
+        vmin,vmax = np.percentile(arr, (plo,phi))
+
+    elif iset == 'r':
+        # Set intensities from min/max range
+        winds = ccd.values()
+        vmin, vmax = winds[0].data.min(), winds[0].data.max()
+        for wind in winds[1:]:
+            vmin = min(vmin, wind.data.min())
+            vmax = max(vmax, wind.data.max())
+
+    elif iset == 'd':
+        vmin, vmax = dlo, dhi
+
+    else:
+        raise HipercamError('mpl.pccd: did not recognise iset = "' +
+                            iset + '"')
+    kwargs['vmin'] = vmin
+    kwargs['vmax'] = vmax
+
+    for key, wind in ccd.iteritems():
+        left,right,bottom,top = wind.extent()
+
+        if 'cmap' in kwargs:
+            axes.imshow(wind.data,extent=(left,right,bottom,top),
+                        aspect=aspect,origin='lower',
+                        interpolation='nearest',**kwargs)
+        else:
+            axes.imshow(wind.data,extent=(left,right,bottom,top),
+                        aspect=aspect,origin='lower',cmap='Greys',
+                        interpolation='nearest',**kwargs)
+        pwin(axes, wind, col, lw)
+
+    return (vmin,vmax)
