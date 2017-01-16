4ad99303daf32351693b9ef9d29db0afe9bea39d
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-01-16 20:07:03 +0000

    changed FITS writing part
    Windata now adds to an HDUList

--- hipercam/window.py
+++ hipercam/window.py
@@ -130,147 +130,7 @@ class Window(object):
         read and editable form of file. "file" is either a file name
         of a file object.
         """
-
-    def wfhead(self, nccd, nwin, fhead, nxccd, nyccd, NX, NY):
-        """
-        Writes window data into a FITS header. Assumes HiPERCAM type
-        data in the sense that the amplifier is computed according to
-        the window number. Non-HiPERCAM data will still work but the
-        amplifier assignment will be wrong.
-
-        Arguments::
-
-          nccd : (int)
-             CCD number, starting from 0
-
-          nwin : (int)
-             Window number, starting from 0
-
-          fhead : (astropy.io.fits.Header)
-             FITS header object. Will be modified on exit.
-
-          nxccd : (int)
-             X dimension of CCD
-
-          nyccd : (int)
-             Y dimension of CCD
-
-          NX : (int)
-             X dimension for iraf mosaic format
-
-          NY : (int)
-             Y dimension for iraf mosaic format
-
-        Returns::
-
-          fhead : (astropy.io.fits.Header)
-             The modified FITS header object.
-        """
-
-        # The next line will likely need modifying
-        namp = nwin % 4 + 1
-
-        fhead['NCCD']    = (nccd+1,'CCD number')
-        fhead['NWIN']    = (nwin+1,'Window number')
-        fhead['LLX']     = (self.llx,  'X-coordinate of lower-left pixel')
-        fhead['LLY']     = (self.lly,  'Y-coordinate of lower-left pixel')
-        fhead['XBIN']    = (self.xbin, 'Binning factor in X')
-        fhead['YBIN']    = (self.ybin, 'Binning factor in Y')
-
-        fhead['CCDNAME'] = (str(nccd+1),'CCD identifier')
-        fhead['AMPNAME'] = (str(namp),'Amplifier identifier')
-        fhead['CCDSIZE'] = ('[1:' + str(nxccd) + ',1:' + str(nyccd) + ']', 'CCD size')
-
-        # Flags to define the amplifier quadrant
-        left   = nwin % 4 == 0 or nwin % 4 == 2
-        bottom = nwin % 4 == 0 or nwin % 4 == 1
-
-        # location within the NX by NY array of CCDs
-        nx = nccd % NX
-        ny = nccd // NY
-
-        # Now we start on the iraf mosaic stuff, see
-        # http://iraf.noao.edu/projects/ccdmosaic/imagedef/imagedef.html
-        # for more detail.
-        # Basically, 4 coordinate systems: CCD, Amplifier, Data, Detector
-        #
-        # CCD  -- basic unbinned 1,1 in lower-left corner
-        # Amp  -- same, but measured with respect to amplifier
-        # Data -- 
-
-        # head
-        ccdsec = ampsec = datasec = detsec = '['
-
-        # x range
-        ccdsec += str(self.llx) + ':' + str(self.llx+self.nx*self.xbin-1)
-        if left:
-            ampsec += str(self.llx) + ':' + str(self.llx+self.nx*self.xbin-1)
-        else:
-            ampsec += str(nxccd-self.llx+1) + ':' + str(nxccd-self.llx-self.nx*self.xbin+2)
-        datasec += '1:' + str(self.nx)
-        detsec  += str(self.llx+nx*nxccd) + ':' + str(self.llx+nx*nxccd+self.nx*self.xbin-1)
-
-        # middle
-        ccdsec += ','
-        ampsec += ','
-        datasec += ','
-        detsec += ','
-
-        # y range
-        ccdsec += str(self.lly) + ':' + str(self.lly+self.nx*self.ybin-1)
-        if bottom:
-            ampsec += str(self.lly) + ':' + str(self.lly+self.ny*self.ybin-1)
-        else:
-            ampsec += str(nyccd-self.lly+1) + ':' + str(nyccd-self.lly-self.ny*self.ybin+2)
-        datasec += '1:' + str(self.ny)
-        detsec  += str(self.lly+ny*nyccd) + ':' + str(self.lly+ny*nyccd+self.ny*self.ybin-1)
-
-        # tail
-        ccdsec += ']'
-        ampsec += ']'
-        datasec += ']'
-        detsec += ']'
-
-
-        fhead['CCDSEC']   = ccdsec
-        fhead['AMPSEC']   = ccdsec
-        fhead['DATASEC']  = datasec
-        fhead['DETSEC']   = detsec
-        fhead['CCDSUM']   = str(self.xbin) + ' ' + str(self.ybin)
-
-#        fhead['CTYPE1']   = 'Pixel'
-#        fhead['CTYPE2']   = 'Pixel'
-#        fhead['CRPIX1']   = 1
-#        fhead['CRPIX2']   = 1
-#        fhead['CRVAL1']   = xref
-#        fhead['CRVAL2']   = yref
-#        fhead['CDELT1']   = self.xbin
-#        fhead['CDELT2']   = self.ybin
-
-        if left:
-            fhead['ATM1_1']   = 1.
-            fhead['ATV1']     = 0.
-        else:
-            fhead['ATM1_1']   = -1.
-            fhead['ATV1']     = nxccd+1
-
-        if bottom:
-            fhead['ATM2_2']   = 1.
-            fhead['ATV2']     = 0.
-        else:
-            fhead['ATM2_2']   = -1.
-            fhead['ATV2']     = nyccd+1
-
-        fhead['LTM1_1']   = 1./self.xbin
-        fhead['LTM2_2']   = 1./self.ybin
-        fhead['LTV1']     = (1-self.llx)/self.xbin
-        fhead['LTV2']     = (1-self.lly)/self.ybin
-        fhead['DTM1_1']   = 1.
-        fhead['DTM2_2']   = 1.
-        fhead['DTV1']     = nx*nxccd
-        fhead['DTV2']     = ny*nyccd
-
-        return fhead
+        print('wjson not implemented yet')
 
     def __eq__(self, win):
         """
@@ -315,6 +175,9 @@ class Windata(Window):
         >>> wind = Windata(win,data)
         >>> wind += 0.5
         >>> wind *= 2
+
+    Note that it is the user's reponsibility to make sure that the data
+    and Window remain compatible.
     """
 
     def __init__(self, win, data=None):
@@ -336,10 +199,12 @@ class Windata(Window):
         else:
             # Run a couple of checks
             if data.ndim != 2:
-                raise HipercamError('Windata.__init__: data must be 2D. Found {0:d}'.format(data.ndim))
+                raise HipercamError(
+                    'Windata.__init__: data must be 2D. Found {0:d}'.format(data.ndim))
             ny, nx = data.shape
             if nx != win.nx or ny != win.ny:
-                raise HipercamError('Windata.__init__: win & data dimensions conflict. NX: {0:d} vs {1:d}, NY: {2:d} vs {3:d}'.format(win.nx,nx,win.ny,ny))
+                raise HipercamError(
+                    'Windata.__init__: win & data dimensions conflict. NX: {0:d} vs {1:d}, NY: {2:d} vs {3:d}'.format(win.nx,nx,win.ny,ny))
 
             self.data = data
 
@@ -423,6 +288,33 @@ class Windata(Window):
           Percentile(s) to use, in range [0,100]
         """
         return np.percentile(self.data, q)
+
+    def whdu(self, fhead=None):
+        """
+        Writes the Windata to an astropy.io.fits.ImageHDU
+
+        Arguments::
+
+          fhead : (astropy.io.fits.Header)
+             An initial FITS header object. The location of the lower-left pixel
+             and the binning factors will be added to it, so it will be modified
+             on exit. 
+
+        Returns::
+
+          hdu : (astropy.io.fits.ImageHDU)
+             The HDU
+        """
+
+        if fhead is None:
+            fhead = fits.Header()
+
+        fhead['LLX'] = (self.llx,  'X-coordinate of lower-left pixel')
+        fhead['LLY'] = (self.lly,  'Y-coordinate of lower-left pixel')
+        fhead['XBIN'] = (self.xbin, 'Binning factor in X')
+        fhead['YBIN'] = (self.ybin, 'Binning factor in Y')
+
+        return fits.ImageHDU(self.data, fhead)
 
     def __iadd__(self, other):
         """+= in-place addition operator. 'other' can be another Windata,
