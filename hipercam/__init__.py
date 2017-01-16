905bb450a6f76da4b933414e8308551ffc450149
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-01-16 14:18:03 +0000

    fixed problem with mpl import
    python3 needs a different sort of import

--- hipercam/__init__.py
+++ hipercam/__init__.py
@@ -5,18 +5,10 @@ hipercam is a package for the reduction of data from the 5-band multi-window
 high-speed CCD camera HiPERCAM.
 """
 
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    from .group import *
-    from .window import *
-    from .ccd import *
-    from .aperture import *
-    import mpl
+from .group import *
+from .window import *
+from .ccd import *
+from .aperture import *
+from . import mpl
 
 
