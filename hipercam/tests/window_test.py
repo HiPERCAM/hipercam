86a6bdc7cfe3e78cfac2927f87dacb6ce322fafc
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-02-01 12:06:31 +0000

    finished set of tests for Window
    every method and attribute now has a test

--- hipercam/tests/window_test.py
+++ hipercam/tests/window_test.py
@@ -1,16 +1,29 @@
 import unittest
+import copy
 
+import numpy as np
 from hipercam import Window, HipercamError
 
 class TestWindow(unittest.TestCase):
+    """
+    Provides simple tests of every Window method and attribute. If any of
+    these fail, something is badly wrong.
+    """
 
     def setUp(self):
         # win1 and win2 should clash (they overlap)
         # win1 is inside win3
+        # win1c is a copy of win1
         self.win1 = Window(3,4,100,200,1,2)
+        self.win1c = copy.copy(self.win1)
         self.win2 = Window(50,50,100,200,1,2)
         self.win3 = Window(1,2,200,300,1,2)
 
+        # win4 is tiny for a quick test of xy; x and y are the
+        # expected returns.
+        self.win4 = Window(11,21,2,3,2,3)
+        self.x = np.array([[11.5,13.5],[11.5,13.5],[11.5,13.5]])
+        self.y = np.array([[22.,22.],[25.,25.],[28.,28.]])
 
     def test_window_llx_value(self):
         self.assertEqual(self.win1.llx, 3,
@@ -63,7 +76,22 @@ class TestWindow(unittest.TestCase):
         self.assertFalse(self.win2.outside(self.win1),
                          'win2 should not be outside win1')
 
+    def test_window_xy(self):
+        x,y = self.win4.xy()
 
+        self.assertTrue((x == self.x).all(),
+                        'incorrect X array returned')
+        self.assertTrue((y == self.y).all(),
+                        'incorrect Y array returned')
+
+    def test_window_matches(self):
+        self.assertRaises(HipercamError, self.win1.matches, self.win2)
+
+    def test_window_eq(self):
+        self.assertTrue(self.win1 == self.win1c)
+
+    def test_window_ne(self):
+        self.assertFalse(self.win1 != self.win1c)
 
 if __name__ == '__main__':
     unittest.main()
