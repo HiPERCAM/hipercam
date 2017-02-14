3a97e554d9aaa82063710b152ae76a90d7f327f1
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-02-14 15:50:29 +0000

    started updating aperture.py

--- hipercam/aperture.py
+++ hipercam/aperture.py
@@ -1,22 +1,17 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-Defines a class to represent a target photometric aperture
+Defines a class to represent a photometric apertures.
 """
-
-# Imports for 2 / 3 compatibility
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-from builtins import *
 
 import numpy as np
 from .core import *
 
 class Aperture(object):
-    """
-    Class representing a photometric aperture for measuring the flux of
-    a star. At its most basic this consists of 3 circles representing
-    the object, radius `r1`, and the sky annulus between radii `r2` and
-    `r3`, centered at a specific position, but there are several extra
+
+    """Class representing a photometric aperture for measuring the flux of a
+    star. At its most basic this consists of 3 circles representing the
+    object, radius `r1`, and the sky annulus between radii `r2` and `r3`,
+    centered at a specific position, but there are several extra
     characteristics in addition. These are:
 
     Reference apertures: these indicate (typically) bright stars that should
@@ -25,15 +20,16 @@ class Aperture(object):
     overall x,y shift is before attempting fainter ones. One indicates this
     status with a logical flag.
 
-    Linked apertures: some targets are nearly impossible to register, or may fade
-    so much that they cannot be detected. Such targets can be "linked" to others
-    in the sense that they are offset from them.
+    Linked apertures: some targets are nearly impossible to register, or may
+    fade so much that they cannot be detected. Such targets can be "linked" to
+    others in the sense that they are offset from them.
 
-    Sky masks: these are fixed circles offset from the aperture in question indicating
-    pixels to ignore when estimating the sky background.
+    Sky masks: these are fixed circles offset from the aperture in question
+    indicating pixels to ignore when estimating the sky background.
 
-    Star masks: these are circles offset from aperture indicating pixels to include when
-    summing the object flux.
+    Star masks: these are circles offset from aperture indicating pixels to
+    include when summing the object flux.
+
     """
 
     def __init__(self, x, y, r1, r2, r3, ref, link=None, mask=None, extra=None):
@@ -90,28 +86,17 @@ class Aperture(object):
         self.mask = extra
 
         # A few elementary checks, but mostly it is left up to
-        # whether it works ...
+        # "duck typing"
         if link is not None and not isinstance(link,list):
-            raise HipercamError('Aperture.__init__: link must be a list')
+            raise ValueError('Aperture.__init__: link must be a list')
 
         if mask is not None and not isinstance(mask,list):
-            raise HipercamError('Aperture.__init__: mask must be a list')
+            raise ValueError('Aperture.__init__: mask must be a list')
 
         if extra is not None and not isinstance(extra,list):
-            raise HipercamError('Aperture.__init__: extra must be a list')
+            raise ValueError('Aperture.__init__: extra must be a list')
 
     def __repr__(self):
-        return 'Aperture(x=' + repr(self.x) + ', y=' + repr(self.y) + \
-            ', r1=' + repr(self.r1) + ', r2=' + repr(self.r2) + \
-            ', r3=' + repr(self.r3) + ', ref=' + repr(self.ref) + \
-            ', link=' + repr(self.link) + ', mask=' + repr(self.mask) + \
-            ', extra=' + repr(self.extra) + ')'
-
-    def clash(self, aper):
-        """
-        Dummy routine to allow `Aperture` objects to be inserted into
-        `Group`s. There are no conditions under which teo `Aperture`
-        objects are incompatible, so this always returns False
-        """
-        return False
-
+        return 'Aperture(x=:{r}, y={:r}, r1={:r}, r2={:r}, r3={:r}, ref={:r}, link={:r}, mask={:r}, extra={:r})'.format(
+            self.x,self.y,self.r1,self.r2,self.r3,
+            self.ref,self.link,self.extra)
