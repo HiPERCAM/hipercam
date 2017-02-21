289dbe891914c037e47ecf5b49779d18f6563a1e
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-02-21 10:11:07 +0000

    revision of data spooling code
    Made Spooler more generic with data source choices set by an integer corresponding to class attributes that can be added to. Method data_source added to carry out the logic of deciding the source given a simple set of inputs. 'rfits' from ccd.py renamed to 'rhcam' in this file.

--- hipercam/spooler.py
+++ hipercam/spooler.py
@@ -1,74 +1,187 @@
-"""Defines a class for feeding out images via a context manager-come-iterator
-to provide a uniform data access.
+"""Defines classes and methods for reading data of varying formats and
+for iterating through multiple images from a variety of sources.
+
 
 """
 
-from .ccd import rfits
+from astropy.io import fits
+from .ccd import CCD, MCCD
 from . import ucam
 
-__all__ = ('Spooler',)
+__all__ = ('Spooler', 'data_source', 'rhcam')
 
-class Spooler:
+def rhcam(fname):
+    """Reads a HiPERCAM file containing either CCD or MCCD data and returns one or
+    the other.
 
-    """A common requirement is the need to loop through a stack of images. The aim
-    of this class is to provide uniform access via an iterable context
-    manager. This allows it to cover 5 cases in the same way: access to raw
-    data via a server or a local disk file, whether from ULTRACAM/SPEC or
-    HiPERCAM, and access to lists of files.
+    Argument::
+
+       fname : (string)
+
+          Name of file. This should be a FITS file with a particular
+          arrangement of HDUs. The keyword HIPERCAM should be present in the
+          primary HDU's header and set to either CCD for single 'CCD' data or
+          'MCCD' for multiple CCDs. A ValueError will be raised if this is not
+          the case. See the docs on CCD and MCCD for more.
 
     """
 
-    # used to indicate which instrument is involved.
-    ULTRA = 1
-    HIPER = 2
+    # Read HDU list
+    with fits.open(fname) as hdul:
+        htype = hdul[0].header['HIPERCAM']
+        if htype == 'CCD':
+            return CCD.rhdul(hdul)
+        elif htype == 'MCCD':
+            return MCCD.rhdul(hdul)
+        else:
+            raise ValueError(
+                'Could not find keyword "HIPERCAM" in primary header of file = {:s}'.format(
+            fname)
+            )
 
-    def __init__(self, run=None, server=False, inst=HIPER,
-                 first=1, flt=True, flist=None):
-        """Sets up info for establishing a source of data frames. Possibilities are
-        (1) via a list of CCD or MCCD files, (2) via a server and (3) from a
-        single raw data file on a local disk. It copes with data from
-        ULTRACAM/SPEC or HiPERCAM.
+def data_source(inst, server=None, flist=True):
+    """Helper routine to return the data source needed by the :class:`Spooler`
+    class given an instrument name and whether access is via a server (else
+    disk) or a file list.
+
+    Arguments::
+
+       inst : (string)
+          Instrument name. Current choices: 'ULTRA' for ULTRACAM/SPEC, 'HIPER'
+          for 'HiPERCAM'.
+
+       server : (bool | None)
+          If server == True, access via a server is expected. If False, a local
+          disk file is assumed. If None, then flist should be set to True.
+
+       flist : (bool | None)
+          If server is None, then flist should be set to True, otherwise it
+          should be set to None or False. 
+
+    Returns::
+
+       An integer corresponding to one of the :class:`Spooler` class attributes
+       representing the supported data sources.
+
+    Exceptions::
+
+       A ValueError will be raised if the inputs are not recognised or
+       conflict.
+    """
+
+    if inst == 'ULTRA':
+        if server is None:
+            raise ValueError(
+                'spooler.data_source: for inst = "ULTRA", server must be True or False')
+        elif server:
+            return Spooler.ULTRA_SERV
+        else:
+            return Spooler.ULTRA_DISK
+
+    elif inst == 'HIPER':
+        if server is None:
+            if flist is None or not flist:
+                raise ValueError(
+                    'spooler.data_source: for inst = "HIPER" and server is None, flist must be True')
+            else:
+                return Spooler.HIPER_LIST
+
+        elif server:
+            return Spooler.HIPER_SERV
+        else:
+            return Spooler.HIPER_DISK
+    else:
+        raise ValueError(
+            'spooler.data_source: inst = {!s} not recognised'.format(inst)
+        )
+
+class Spooler:
+
+    """A common requirement is the need to loop through a stack of images. With a
+    variety of possible data sources, one requires handling of multiple
+    possible ways of accessing the data. The aim of this class is to provide
+    uniform access via an iterable context manager.
+
+    """
+
+    # supported types of data access
+    HIPER_DISK = 1 # HiPERCAM raw data from a local disk file
+    HIPER_SERV = 2 # HiPERCAM raw data from a server
+    HIPER_LIST = 3 # HiPERCAM from list of hcm files
+    ULTRA_DISK = 4 # ULTRACAM/SPEC raw data from a local disk file
+    ULTRA_SERV = 5 # ULTRACAM/SPEC raw data from a server
+
+    def __init__(self, ident, source=HIPER_SERV, first=1, flt=False):
+        """Sets up info for establishing a source of data frames.
 
         Arguments::
 
-           run : (string | None)
-              If not None, this must contain the run number as in 'run003' to
-              access, whether via a server or the local disk.
+           ident : (string)
 
-           server : (bool | None)
-              If run is not None, then this must be a bool, True for file
-              access via a server, False for file accessed from local disk.
+              An identifier of the data source whose nature depends on the
+              value of `source`. For instance if source == HIPER_DISK, this
+              should be a run number e.g. 'run003' or 'data/run004'. If source
+              == HIPER_LIST then it should be the name of a file list.
 
-           inst : (string)
-              If run is not None, this must be set to either Spooler.ULTRA or
-              Spooler.HIPER for ULTRACAM/SPEC vs HiPERCAM as the data source.
+           source : (int)
+              Data source. The possibilities are defined by a set of class
+              attributes such as `HIPER_DISK` and `ULTRA_SERV`. See below
+              for a full listing of their meanings. You will need to specify
+              the class as in `Spooler.ULTRA_DISK`.
 
            first : (int)
-              If run is not None, the first frame to access.
+              The first frame to access (ignored for the file list sources on
+              the basis that lists can be tuned to suit usage)
 
            flt : (bool)
-              If run is not None, flt=True/False determines whether the raw
-              data are converted to 4-bytes floats on input or not.
+              If True, a conversion to 4-byte floats on input will be attempted
+              if the data are of integer form.
 
-           flist : (string | None)
-              If run is None, this must be set to the name of a list of files.
+        Current source options and their implications for the meaning of
+        `ident`::
+
+           HIPER_DISK : HiPERCAM raw data from a local disk file. `ident`
+                        should specify the run in this case.
+
+           HIPER_SERV : HiPERCAM raw data from a server. `ident` should
+                        specify the run; the server name will be grabbed from
+                        an environment variable `HIPERCAM_DEFAULT_URL`
+
+           HIPER_LIST : HiPERCAM from list of hcm files. `ident` should
+                        specify the name of a list of .hcm files.
+
+           ULTRA_DISK : ULTRACAM/SPEC raw data from a local disk file. `ident`
+                        should specify the run.
+
+           ULTRA_SERV : ULTRACAM/SPEC raw data from a server. `ident` should
+                        specify the run; the server name will be grabbed from
+                        an environment variable `ULTRACAM_DEFAULT_URL`
 
         """
 
-        self.run = run
-        self.server = server
-        self.inst = inst
-        self.first = first
-        self.flt = flt
-        self.flist = flist
+        if source == Spooler.HIPER_DISK:
+            raise ValueError(
+                'Spooler.__init__: source == HIPER_DISK not yet supported')
 
-        if self.run is None:
-            self._iter = open(flist)
+        elif source == Spooler.HIPER_SERV:
+            raise ValueError(
+                'Spooler.__init__: source == HIPER_SERV not yet supported')
+
+        elif source == Spooler.ULTRA_DISK:
+            self._iter = ucam.Rdata(run, first, flt, False)
+
+        elif source == Spooler.ULTRA_SERV:
+            self._iter = ucam.Rdata(run, first, flt, True)
+
+        elif source == Spooler.HIPER_LIST:
+            self._iter = open(ident)
+
         else:
-            if self.inst == Spooler.ULTRA:
-                self._iter = ucam.Rdata(run, first, flt, server)
-            else:
-                raise ValueError('Spooler.__init__: unrecognised argument combination')
+            raise ValueError(
+                'Spooler.__init__: source == {:d} not recognised'.format(source)
+            )
+
+        self.source = source
 
     # next two define the actions associated with the context manager
     def __enter__(self):
@@ -82,13 +195,13 @@ class Spooler:
         return self
 
     def __next__(self):
-        if self.run is None:
-            # running off a file list
+        if self.source == Spooler.HIPER_LIST:
             for fname in self._iter:
                 if not fname.startswith('#'):
                     break
             else:
                 raise StopIteration
-            return rfits(fname.strip())
+
+            return rhcam(fname.strip())
         else:
             return self._iter.__next__()
