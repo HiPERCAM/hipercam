f85c5c0a14cdcc35cc48883ff593e396c9393b24
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-09-27 11:17:59 +0100

    building up the Aperture class to represent photometry apertures.
    added several basic methods, changed arguments, put in some json serialisation code.

--- hipercam/aperture.py
+++ hipercam/aperture.py
@@ -4,6 +4,8 @@ Defines a class to represent a photometric apertures.
 """
 
 import numpy as np
+import json
+from collections import OrderedDict
 from .core import *
 
 class Aperture(object):
@@ -17,8 +19,8 @@ class Aperture(object):
     Reference apertures: these indicate (typically) bright stars that should
     be easy to locate. The idea is that when re-positioning apertures, you
     might want to initially do the brightest stars, and then see what the
-    overall x,y shift is before attempting fainter ones. One indicates this
-    status with a logical flag.
+    overall x,y shift is before attempting fainter ones. This status is
+    indicated with a logical flag.
 
     Linked apertures: some targets are nearly impossible to register, or may
     fade so much that they cannot be detected. Such targets can be "linked" to
@@ -32,7 +34,7 @@ class Aperture(object):
 
     """
 
-    def __init__(self, x, y, r1, r2, r3, ref, link=None, mask=None, extra=None):
+    def __init__(self, x, y, r1, r2, r3, ref, link=None, mask=[], extra=[]):
         """
         Constructor. Arguments::
 
@@ -58,9 +60,9 @@ class Aperture(object):
             aperture meaning that its position will be re-determined
             before non-reference apertures to provide a shift.
 
-        link : (key value)
-            If set, this gives a lookup key to find the aperture this is
-            linked from. Set to `None` for no link.
+        link : (Aperture / None)
+            If set, this is an Aperture that *this* Aperture is linked from.
+            Set to `None` for no link.
 
         mask : (list of 3 element tuples)
             Each tuple in the list consists of an x,y offset and a radius in
@@ -73,6 +75,9 @@ class Aperture(object):
             when summing the total flux and allow one to add in extra regions,
             typically blended stars that cannot be allowed to straddle the
             edge of the inner target aperture.
+
+        Notes: normal practice would be to set link, mask, extra later, having created
+        the Aperture
         """
 
         self.x = x
@@ -81,22 +86,64 @@ class Aperture(object):
         self.r2 = r2
         self.r3 = r3
         self.ref = ref
-        self.link = link
-        self.mask = mask
-        self.mask = extra
-
-        # A few elementary checks, but mostly it is left up to
-        # "duck typing"
-        if link is not None and not isinstance(link,list):
-            raise ValueError('Aperture.__init__: link must be a list')
-
-        if mask is not None and not isinstance(mask,list):
-            raise ValueError('Aperture.__init__: mask must be a list')
-
-        if extra is not None and not isinstance(extra,list):
-            raise ValueError('Aperture.__init__: extra must be a list')
+        self._link = link
+        self._mask = mask
+        self._extra = extra
 
     def __repr__(self):
-        return 'Aperture(x=:{r}, y={:r}, r1={:r}, r2={:r}, r3={:r}, ref={:r}, link={:r}, mask={:r}, extra={:r})'.format(
-            self.x,self.y,self.r1,self.r2,self.r3,
-            self.ref,self.link,self.extra)
+        return 'Aperture(x={!r}, y={!r}, r1={!r}, r2={!r}, r3={!r}, ref={!r}, link={!r}, mask={!r}, extra={!r})'.format(
+            self.x,self.y,self.r1,self.r2,self.r3,self.ref,self._link,self._mask,self._extra
+            )
+
+    def add_mask(self, xoff, yoff, radius):
+        """Adds a mask to the :class:Aperture"""
+        self._mask.append((xoff,yoff,radius))
+
+    def add_extra(self, xoff, yoff, radius):
+        """Adds a mask to the :class:Aperture"""
+        self._extra.append((xoff,yoff,radius))
+
+    def set_link(self, aperture):
+        """Links this :class:Aperture to another"""
+        self._link = aperture
+
+    def break_link(self):
+        """Cancels any link to another :class:Aperture"""
+        self._link = None
+
+    def toJson(self, fp):
+        """Write in JSON-format to file pointer fp"""
+        json.dump(self, fp, cls=_encoder, indent=2)
+
+    @classmethod
+    def fromJson(cls, fp):
+        """Read from JSON-format file pointer fp"""
+        return json.load(fp, cls=_decoder)
+
+
+# classes to support JSON serialisation
+class _encoder(json.JSONEncoder):
+    def default(self, aperture):
+        return OrderedDict((
+                ('Comment', 'This is an Aperture object'),
+                ('x', aperture.x),
+                ('y', aperture.y),
+                ('r1', aperture.r1),
+                ('r2', aperture.r2),
+                ('r3', aperture.r3),
+                ('link', aperture._link),
+                ('mask', aperture._mask),
+                ('extra', aperture._extra),
+                ))
+
+class _decoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, jobj):
+        if 'r1' in jobj and 'r2' in jobj and 'r3' in jobj and 'link' in jobj:
+            return Aperture(jobj['x'],jobj['y'],jobj['r1'],jobj['r2'],jobj['r3'],jobj['link'],jobj['mask'],jobj['extra'])
+        else:
+            return jobj
+
