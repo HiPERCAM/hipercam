8e84552bc60bc0d9c1f0b2260e3960c46022537e
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-09-29 18:28:17 +0100

    Imports docs from all the import * stuff
    Reveals doc strings at the top level for those objects imported there. Also added more introduction.

--- hipercam/__init__.py
+++ hipercam/__init__.py
@@ -5,10 +5,46 @@ reduction package for the high-speed camera HiPERCAM
 
 hipercam is a package for the reduction of data from the 5-band multi-window
 high-speed CCD camera HiPERCAM. It can access the raw data, display it, slice
-it into individual files containing the 5 CCDs, etc.
+it into individual files containing the 5 CCDs, etc. It provides a combination
+of an API to access HiPERCAM data and high-level scripts to enact certain
+commonly-required operations in reduction. Most users will be most interested
+in the latter, and if that is the case for you, your first port of entry is
+probably to look at the 'scripts' subpackage, e.g. pydoc hipercam.scripts.
 
-When using it for the first time, your first port of entry is probably to look
-at the 'scripts' subpackage, e.g. pydoc hipercam.scripts
+If you are interested in the API, then read on. The code is compartmentalised
+into several sub-modules and sub-packages, e.g. :class:`CCD` and :class:`MCCD`
+appear in hipercam.ccd, but they are also imported to the top level, so that
+e.g.  'pydoc hipercam.CCD' and 'pydoc hipercam.ccd.CCD' report back the same
+thing. This is trus for most of the sub-modules, so looking through the
+top-level docs covers a large part of the API. However, it is rather long and
+if you want to specialise to a restricted part of the documentation, then a
+command like 'pydoc hipercam.ccd' can prove useful to find a more focussed
+view of related software. There are some exceptions though where you will need
+to drill deeper, namely the sub-packages like 'ucam' and 'hcam', the
+sub-module hipercam.cline, and the plotting sub-sections, 'mpl' and 'pgp'.
+
+Perhaps the most important class of all is :class:`MCCD` which is the first
+place to go if you have a HiPERCAM '.hcm' file you want to do something with
+beyond the current routines (although NB '.hcm' files are just FITS files in
+thin disguise of a changed extension and you may be able to do your own stuff
+with standard tools for FITS files). Example code:
+
+  >> import hipercam as hcam
+  >> mccd = hcam.MCCD.rfits('myfile.hcm')
+  >> print(mccd)
+  >> print(mccd['1'])
+  >> print(mccd['1']['E1'])
+  >> print(mccd['1']['E1'][123,45])
+  >> mccd['1']['E1'][123,45] = 0
+  >> mccd.wfits('mynewfile.hcm')
+
+which reads an hcm file in, then prints it, the CCD it contains labelled '1'
+(a :class:`CCD` object), then the window data from this CCD labelled 'E1' (a
+:class:`Windata` object) and finally the pixel value at iy=123, ix=45 (C-like
+starting from 0). This shows the dictionary-like interface used for
+:class:`MCCD` and :class:`CCD` objects alike (they are both sub-classes of a
+container class :class:`Group`). It then sets this pixel to zero and writes
+out the result to a new file.
 """
 
 from .core import *
@@ -23,3 +59,7 @@ from . import pgp
 from . import ucam
 from . import hcam
 from .spooler import *
+
+__all__ = core.__all__ + group.__all__ + window.__all__ + ccd.__all__ + \
+    aperture.__all__ + target.__all__ + spooler.__all__
+
