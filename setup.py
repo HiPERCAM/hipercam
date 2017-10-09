e4be933b191d20b31712668a4407851708b2fb16
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-10-09 22:51:46 +0100

    added comment about numpy version needed

--- setup.py
+++ setup.py
@@ -71,6 +71,9 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy','astropy','matplotlib'],
 
+    # need numpy version 1.10 for numpy.stack,
+    # 1.12 for numpy.flip and num
+
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
