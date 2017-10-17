6a2b0a2e37f231abf92ab689a3bf5cf67386911f
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-10-17 12:47:31 +0100

    improved the reduce file quick look script
    now allows you to plot any parameter in combination with or versus any other.

--- hipercam/scripts/plog.py
+++ hipercam/scripts/plog.py
@@ -34,23 +34,35 @@ def plog(args=None):
          plot height (inches). Set = 0 to let the program choose. BOTH width
          AND height must be non-zero to have any effect
 
-      ccd    : (string)
-         CCD(s) to plot, '0' for all. If not '0' then '1', '2' or even '3 4'
-         are possible inputs (without the quotes). '3 4' will plot CCD '3' and
-         CCD '4'. 
+      ccd1   : (string)
+         first CCD to consider, e.g. '1'
 
-      targ   : (string)
-         target aperture
+      aper1   : (string)
+         first aperture to consider
 
-      comp   : (string)
-         comparison aperture, 'none' to ignore.
+      param1   : (string)
+         first parameter to consider. Choices are 'x' = X position,
+         'y' = Y position, 'f' = FWHM, 'b' = Moffat beta, 's' = sky.
 
-      param  : (string)
-         parameter to plot
+      ccd2   : (string)
+         second CCD to consider; '!' to ignore. Can be (and typically
+         would be) the same as ccd1.
 
-      scheme : (string) [if 'comp' not none]
-         how to plot 'targ' and 'comp'. 'd' = difference;
-         'i' = ignore; 'r' = ratio; 's' = scatter plot.
+      aper2   : (string) [if ccd2 != '!']
+         second aperture to consider
+
+      param2   : (string) [if ccd2 != '!']
+         second parameter. See param1 for choices
+
+      scheme   : (string) [if ccd2 != '!']
+         how to plot if both apertures are chosen. Choices:
+         'd' = difference, i.e. plot 1-2; 'b' = both plotted on same
+         panel; 'r' = ratio, i.e. 1 / 2, good for relative photom;
+         's' = scatter plot, 2 vs 1.
+
+    NB Points with negative errors are ignored. Be careful with linked
+    apertures where all x, y, FWHM, beta automatically have negative
+    errors since they are not fitted.
     """
 
     if args is None:
@@ -64,10 +76,12 @@ def plog(args=None):
         cl.register('device', Cline.LOCAL, Cline.HIDE)
         cl.register('width', Cline.LOCAL, Cline.HIDE)
         cl.register('height', Cline.LOCAL, Cline.HIDE)
-        cl.register('ccd', Cline.LOCAL, Cline.PROMPT)
-        cl.register('targ', Cline.LOCAL, Cline.PROMPT)
-        cl.register('comp', Cline.LOCAL, Cline.PROMPT)
-        cl.register('param', Cline.LOCAL, Cline.PROMPT)
+        cl.register('ccd1', Cline.LOCAL, Cline.PROMPT)
+        cl.register('aper1', Cline.LOCAL, Cline.PROMPT)
+        cl.register('param1', Cline.LOCAL, Cline.PROMPT)
+        cl.register('ccd2', Cline.LOCAL, Cline.PROMPT)
+        cl.register('aper2', Cline.LOCAL, Cline.PROMPT)
+        cl.register('param2', Cline.LOCAL, Cline.PROMPT)
         cl.register('scheme', Cline.LOCAL, Cline.PROMPT)
 
         # get inputs
@@ -77,22 +91,30 @@ def plog(args=None):
         )
 
         device = cl.get_value('device', 'plot device name', 'term')
-        print('device = ',device)
         width = cl.get_value('width', 'plot width (inches)', 0.)
         height = cl.get_value('height', 'plot height (inches)', 0.)
 
-        ccd = cl.get_value('ccd', 'CCD(s) to plot [0 for all]', '0')
-
-        targ = cl.get_value('targ', 'target aperture', '1')
-
-        comp = cl.get_value(
-            'comp', "comparison aperture ['!' to ignore]", '2'
-        )
-
-        param = cl.get_value(
-            'param', 'parameter [x, y, f(whm), b(eta), c(ounts), s(ky)]',
+        ccd1 = cl.get_value('ccd1', 'first CCD to plot', '1')
+        aper1 = cl.get_value('aper1', 'first aperture', '1')
+        param1 = cl.get_value(
+            'param1', 'first parameter [x, y, f(whm), b(eta), c(ounts), s(ky)]',
             'c', lvals=('x','y','f','b','c','s')
         )
+        lab1 = '[CCD={:s},ap={:s},p={:s}]'.format(
+            ccd1, aper1, param1)
+
+        ccd2 = cl.get_value('ccd2', "second CCD to plot ['!' to ignore]", ccd1)
+        if ccd1 != '!':
+            aper2 = cl.get_value('aper2', 'second aperture', '1')
+            param2 = cl.get_value(
+                'param2',
+                'second parameter [x, y, f(whm), b(eta), c(ounts), s(ky)]',
+                'c', lvals=('x','y','f','b','c','s')
+            )
+            lab2 = '[CCD={:s},ap={:s},p={:s}]'.format(
+                ccd2, aper2, param2)
+
+        # map to names used in reduce log files
         MAP = {
             'x' : 'x',
             'y' : 'y',
@@ -101,54 +123,65 @@ def plog(args=None):
             'c' : 'counts',
             's' : 'sky'
         }
-        pname = MAP[param]
+        pname1 = MAP[param1]
+        pname2 = MAP[param2]
 
-        if comp != '!':
+        if ccd2 != '!':
             scheme = cl.get_value(
-                'scheme', 'd(ifference), i(gnore), r(atio), s(catter)',
-                'i', lvals=('d','i','r','s')
+                'scheme', 'b(oth), d(ifference), r(atio), s(catter)',
+                'b', lvals=('b','d','r','s')
             )
 
     # load the reduce log
     hlog = hcam.hlog.Hlog.fromLog(log)
-
-    if ccd == '0':
-        ccds = list(hlog.keys())
-    else:
-        ccds = ccd.split()
 
     if width > 0 and height > 0:
         fig = plt.figure(figsize=(width,height))
     else:
         fig = plt.figure()
 
-#    mpl.rcParams['xtick.labelsize'] = hcam.mpl.Params['axis.number.fs']
-#    mpl.rcParams['ytick.labelsize'] = hcam.mpl.Params['axis.number.fs']
+    dat1 = hlog.tseries(ccd1, aper1, pname1)
+    if ccd2 != '!':
+        dat2 = hlog.tseries(ccd2, aper2, pname2)
+        if scheme == 'b':
+            # plots both together
+            dat1.mplot(plt,'b')
+            dat2.mplot(plt,'r')
+            xlabel = 'Time [MJD]'
+            ylabel = '{:s} & {:s}'.format(lab1, lab2)
 
-    for cnam in ccds:
-        tdat = hlog.tseries(cnam, targ, pname)
-        if comp != '!':
-            cdat = hlog.tseries(cnam, comp, pname)
-            if scheme == 'i':
-                tdat.mplot(plt)
-                cdat.mplot(plt)
+        elif scheme == 'r':
+            # ratio
+            ratio = dat1 / dat2
+            ratio.mplot(plt,'b')
+            xlabel = 'Time [MJD]'
+            ylabel = '{:s} / {:s}'.format(lab1, lab2)
 
-            elif scheme == 'r':
-                ratio = tdat / cdat
-                ratio.mplot(plt)
+        elif scheme == 'd':
+            # difference
+            diff = dat1 - dat2
+            diff.mplot(plt,'b')
+            xlabel = 'Time [MJD]'
+            ylabel = '{:s} - {:s}'.format(lab1, lab2)
 
-            elif scheme == 'd':
-                diff = tdat - cdat
-                diff.mplot(plt)
-
-            elif scheme == 's':
-                ok = (tdat.ye > 0) & (cdat.ye > 0)
-                plt.errorbar(
-                    tdat.y[ok], cdat.y[ok], cdat.ye[ok], tdat.ye[ok],
+        elif scheme == 's':
+            ok = (dat1.ye > 0) & (dat2.ye > 0)
+            plt.errorbar(
+                dat1.y[ok], dat2.y[ok], dat2.ye[ok], dat1.ye[ok],
                     '.', capsize=0
                 )
+            xlabel = lab1
+            ylabel = lab2
+
         else:
-            tdat.mplot(plt)
+            # just one
+            dat1.mplot(plt)
+            xlabel = 'Time [MJD]'
+            ylabel = lab1
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(log)
 
     if device == 'term':
         plt.show()
