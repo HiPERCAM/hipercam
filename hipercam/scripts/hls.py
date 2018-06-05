ae030c94cd36da272a08277e4a3b84304fdb0f44
Tom Marsh <t.r.marsh@warwick.ac.uk>
2018-06-05 11:53:42 +0100

    fixed hls to work with ULTRACAM server
    Hope I haven't broken it as far as the HiPERCAM server goes, but I don't think I have.

--- hipercam/scripts/hls.py
+++ hipercam/scripts/hls.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 from urllib.request import urlopen
 
 __all__ = ['hls',]
@@ -15,9 +16,18 @@ URL = 'http://{:s}'.format(os.environ['HIPERCAM_DEFAULT_URL']) \
       if 'HIPERCAM_DEFAULT_URL' in os.environ else \
          'http://localhost:8007/'
 
+# need to pick up file names from either the hipercam or the ultracam
+# server. The hipercam server simply returns a list of files in the form
+# run####, one per line whereas the ultracam server surrounds it with html
+# stuff.
+fre = re.compile(r'^(run\d\d\d\d)$|>(run\d\d\d)<')
+
 def hls(args=None):
-    """Gives an 'ls'-like listing of the runs available on the HiPERCAM
-    server. Just invoke as 'hls' with no arguments.
+    """Gives an 'ls'-like listing of the runs available on the HiPERCAM file
+    server. Just invoke as 'hls' with no arguments. This should work correctly
+    whether it in fact its the HiPERCAM or ULTRACAM file server that is
+    running.
+
     """
 
     url = '{:s}?action=dir'.format(URL)
@@ -26,15 +36,18 @@ def hls(args=None):
         response = f.read().decode('utf-8')
 
     # splits up response, searches for file names of the
-    # form runXXXX, prints them as a list.
-    fnames = [fname for fname in response.split('\n') \
-              if fname.startswith('run') and len(fname) == 7]
+    # form runXXX[X], prints them as a list. If it finds
+    fnames = []
+    for line in response.split('\n'):
+        m = fre.search(line)
+        if m:
+            fnames.append(m.group(0) if m.group(1) else m.group(2))
 
     # sort
     fnames.sort()
 
     # output to terminal
-    print('\n'.join(fnames))
-
-
-
+    if len(fnames):
+        print('\n'.join(fnames))
+    else:
+        print('No runs returned by server')
