bfcfe4250a3694f845a2fc99a05eeac8d9b656e9
Tom Marsh <phsaap@localhost.localdomain>
2021-05-31 00:37:52 +0100

    working version of hpackage
    but needs some further testing

--- hipercam/scripts/hpackage.py
+++ hipercam/scripts/hpackage.py
@@ -3,6 +3,7 @@ import os
 import signal
 import re
 import shutil
+import subprocess
 
 import numpy as np
 from astropy.time import Time
@@ -27,52 +28,75 @@ __all__ = [
 
 
 def hpackage(args=None):
-    """``hpackage run``
+    """``hpackage runs label``
 
-    ``hpackage`` looks for standard data products run#.hcm, run#.ape,
-    run#.red, run#.log, (where # is an integer identifier), spits out
-    joined up images of the CCDs in run#.hcm, converts the log to
-    fits, and writes out a "region" file representing the apertures
-    readable by ds9. It then tars the whole lot up. For simplicity, it
-    deliberately runs with a minimal set or arguments: the run
-    name. It must be run in the directory with all the files.
+    ``hpackage`` looks for standard reduce data products and bundles
+    them up for users. The idea is to copy all the files needed to be
+    bale to re-run the reduction with the pipeline, while also adding
+    a few helpfule extras. Given 'run123' for instance, it looks for:
+
+      run123.hcm -- typically the result from a run of |averun|
+      run123.ape -- file of photometric apertures
+      run123.red -- reduce file as made by |genred|
+      run123.log -- result from |reduce|
+
+    It also looks for calibration files inside the reduce file and
+    copies them. It requires them to be within the same direcory and
+    will fail if they are not.
+
+    It produces several extra files which are:
+
+      run123.fits -- FITS version of the log file
+      run123_ccd1.fits -- joined-up ds9-able version of run123.hcm
+                          (and ccd2 etc)
+      run123_ccd1.reg -- ds9-region file representing the apertures
+                         from run123.ape
+
+    Arguments:
+
+      run : str
+         Series of run names of the ones to copy, separated by spaces.
+
+      label : str
+         Name for the directory to store all files forming root of
+         output tar file
 
     """
 
     command, args = utils.script_args(args)
+    FEXTS = (hcam.HCAM, hcam.APER, hcam.LOG, hcam.RED)
 
     # get the inputs
     with Cline("HIPERCAM_ENV", ".hipercam", command, args) as cl:
 
         # register parameters
-        cl.register("run", Cline.LOCAL, Cline.PROMPT)
-        run = cl.get_value("run", "run name", cline.Fname("run005", hcam.HCAM))
-        if os.path.dirname(run) != '':
-            raise hcam.HipercamError(
-                'hpackage only runs on files in the working directory'
-            )
+        cl.register("runs", Cline.LOCAL, Cline.PROMPT)
+        cl.register("label", Cline.LOCAL, Cline.PROMPT)
 
-    # strip extension
-    root = os.path.splitext(run)[0]
-
-    nre = re.compile('(\d\d\d\d)[-_](\d\d)[-_](\d\d)')
-    cwd = os.getcwd()
-    m = nre.search(cwd)
-    if m is None:
-        raise hcam.HipercamError(
-            'hpackage must be run in a sub-directory'
-            ' of a night directory [YYYY[-_]MM[-_]DD]'
+        runs = cl.get_value(
+            "runs", "run names [space separated]",
+            'run005'
         )
+        runs = runs.split()
+        for run in runs:
+            if os.path.dirname(run) != '':
+                raise hcam.HipercamError(
+                    'hpackage only runs on files in the working directory'
+                )
+            for fext in FEXTS:
+                if not os.path.exists(run + fext):
+                    raise hcam.HipercamError(
+                        f'could not find {run+fext}'
+                    )
 
-    # Finally, read the hcm file
-    mccd = hcam.MCCD.read(run)
-
-    # check for existence of various files
-    night_run = f'{m.group(1)}-{m.group(2)}-{m.group(3)}-{root}'
+        label = cl.get_value(
+            "label", "label for output tar file",
+            'hdata'
+        )
 
     # Make name of temporary directory
     tdir = utils.temp_dir()
-    tmpdir = os.path.join(tdir, night_run)
+    tmpdir = os.path.join(tdir, label)
 
     with CleanUp(tmpdir) as cleanup:
 
@@ -80,123 +104,115 @@ def hpackage(args=None):
         os.makedirs(tmpdir, exist_ok=True)
         print(f'Will write files to {tmpdir}')
 
-        # copy standard files over
-        for ext in (hcam.APER,hcam.LOG,hcam.RED):
-            source = utils.add_extension(root,ext)
-            target = os.path.join(tmpdir,source)
-            shutil.copyfile(source, target)
-            print(f'copied {source} to {target}')
+        # Get on
+        for run in runs:
 
-        for cnam, ccd in mccd.items():
+            # strip extension
+            root = os.path.splitext(run)[0]
 
-            # We need to generate a single data array for all
-            # data. First check that it is even possible
+            # need to read the file to determine
+            # the number of CCDs
+            print(
+                run,root,utils.add_extension(run,hcam.HCAM)
+            )
+            mccd = hcam.MCCD.read(
+                utils.add_extension(run,hcam.HCAM)
+            )
 
-            for n, wind in enumerate(ccd.values()):
-                if n == 0:
-                    llx = llxmin = wind.llx
-                    lly = llymin = wind.lly
-                    urxmax = wind.urx
-                    urymax = wind.ury
-                    xbin = wind.xbin
-                    ybin = wind.ybin
-                else:
-                    # Track overall dimensions
-                    llxmin = min(llxmin, wind.llx)
-                    llymin = min(llymin, wind.lly)
-                    urxmax = max(urxmax, wind.urx)
-                    urymax = max(urymax, wind.ury)
-                    if xbin != wind.xbin or ybin != wind.ybin:
-                        raise hcam.HipercamError(
-                            'Found windows with clashing binning factors'
+            # convert the  hcm and ape files using joinup
+            args = [
+                None,'prompt','list','hf',run,'no'
+            ]
+            if len(mccd) > 1:
+                args += ['0']
+            args += [
+                root + hcam.APER,
+                'none','none','none','none','no',
+                'float32',str(100),str(100),'no','rice',
+                tmpdir
+            ]
+            hcam.scripts.joinup(args)
+
+            # convert log to fits as well
+            args = [
+                None,'prompt',run,'h',tmpdir
+            ]
+            hcam.scripts.hlog2fits(args)
+
+            # copy standard files over
+            for fext in FEXTS:
+                source = utils.add_extension(root,fext)
+                target = os.path.join(tmpdir,source)
+                shutil.copyfile(source, target)
+                print(f'copied {source} to {target}')
+
+            # now the calibrations
+            rfile = hcam.reduction.Rfile.read(run + hcam.RED)
+            if rfile.bias is not None:
+                source = utils.add_extension(
+                    rfile.bias.head['FILENAME'], hcam.HCAM
+                )
+                if os.path.dirname(source) != '':
+                    raise HipercamError(
+                        f'bias = {source} is not in the present working directory'
+                    )
+                target = os.path.join(tmpdir,source)
+                shutil.copyfile(source, target)
+                print(f'copied {source} to {target}')
+
+            if rfile.dark is not None:
+                source = utils.add_extension(
+                    rfile.dark.head['FILENAME'], hcam.HCAM
+                )
+                if os.path.dirname(source) != '':
+                    raise HipercamError(
+                        f'dark = {source} is not in the present working directory'
+                    )
+                target = os.path.join(tmpdir,source)
+                shutil.copyfile(source, target)
+                print(f'copied {source} to {target}')
+
+            if rfile.flat is not None:
+                source = utils.add_extension(
+                    rfile.flat.head['FILENAME'], hcam.HCAM
+                )
+                if os.path.dirname(source) != '':
+                    raise HipercamError(
+                        f'flat = {source} is not in the present working directory'
+                    )
+                target = os.path.join(tmpdir,source)
+                shutil.copyfile(source, target)
+                print(f'copied {source} to {target}')
+
+            if rfile.fmap is not None:
+
+                source = utils.add_extension(
+                    rfile.fmap.head['FILENAME'], hcam.HCAM
+                )
+                if os.path.dirname(source) != '':
+                    raise HipercamError(
+                        f'fringe map = {source} is not in the present working directory'
+                    )
+                target = os.path.join(tmpdir,source)
+                shutil.copyfile(source, target)
+                print(f'copied {source} to {target}')
+
+                if rfile.fpair is not None:
+
+                    source = utils.add_extension(
+                        rfile.fpair.head['FILENAME'], hcam.HCAM
+                    )
+                    if os.path.dirname(source) != '':
+                        raise HipercamError(
+                            f'fringe peak/trough pair file = {source} is not in the present working directory'
                         )
+                    target = os.path.join(tmpdir,source)
+                    shutil.copyfile(source, target)
+                    print(f'copied {source} to {target}')
 
-                    if (wind.llx - llx) % xbin != 0 or \
-                       (wind.lly - lly) % ybin != 0:
-                        raise hcam.HipercamError(
-                            'Found windows which are out '
-                            'of sync with each other'
-                        )
-
-            # create huge array of nothing
-            ny = (urymax-llymin+1) // ybin
-            nx = (urxmax-llxmin+1) // xbin
-            data = np.zeros((ny,nx))
-
-            # fill it with data
-            for n, wind in enumerate(ccd.values()):
-                xstart = (wind.llx - llxmin) // xbin
-                ystart = (wind.lly - llymin) // ybin
-                data[ystart:ystart+wind.ny,xstart:xstart+wind.nx] = wind.data
-            data = data.astype(np.float32)
-
-            # Header
-            phead = mccd.head.copy()
-
-            # Add some extra stuff
-            phead["CCDLABEL"] = (cnam, "CCD label")
-            phead["LLX"] = (
-                llxmin,
-                "X of lower-left unbinned pixel (starts at 1)"
-            )
-            phead["LLY"] = (
-                llymin,
-                "Y of lower-left unbinned pixel (starts at 1)"
-            )
-            phead["NXTOT"] = (ccd.nxtot, "Total unbinned X dimension")
-            phead["NYTOT"] = (ccd.nytot, "Total unbinned Y dimension")
-            phead.add_comment('Written by HiPERCAM script "hpackage"')
-
-            # Make header
-            header=fits.Header(phead.cards)
-
-            # attempt to generate a WCS
-            instrume = header.get('INSTRUME','UNKNOWN')
-            if instrume == 'HIPERCAM' and 'RADEG' in header and \
-               'DECDEG' in header and 'INSTRPA' in header:
-
-                ZEROPOINT = 69.3 # rotator zeropoint
-                SCALE = 0.081 # pixel scale, "/unbinned pixel
-
-                # ra, dec in degrees at rotator centre
-                ra = header['RADEG']
-                dec = header['DECDEG']
-
-                # position angle, degrees
-                pa = header['INSTRPA'] - ZEROPOINT
-
-                # position within binned array of rotator centre
-                x0, y0 = (1020.-llxmin+1)/xbin, (524.-llymin+1)/ybin
-
-                # Build the WCS
-                w = wcs.WCS(naxis=2)
-                w.wcs.crpix = [x0, y0]
-                w.wcs.crval = [ra,dec]
-                cpa = np.cos(np.radians(pa))
-                spa = np.sin(np.radians(pa))
-                cd = np.array([[xbin*cpa,-ybin*spa],[xbin*spa,ybin*cpa]])
-                cd = np.array([[xbin*cpa,ybin*spa],[-xbin*spa,ybin*cpa]])
-                cd *= SCALE/3600
-                w.wcs.cd = cd
-                w.wcs.ctype = ['RA---TAN','DEC--TAN']
-                w.wcs.cunit = ["deg", "deg"]
-                header.update(w.to_header())
-                print(f'CCD {cnam}: added WCS')
-            else:
-                print(f'CCD {cnam}: missing positional data; no WCS added')
-
-            # make the first & only HDU
-            hdul = fits.HDUList()
-            hdul.append(fits.PrimaryHDU(header=header))
-            compressed_hdu = fits.CompImageHDU(
-                data=data, compression_type='RICE_1'
-            )
-            hdul.append(compressed_hdu)
-
-            oname = os.path.join(tmpdir, f'{root}_ccd{cnam}.fits')
-
-            hdul.writeto(oname)
-            print(f'CCD {cnam}: written to {oname}')
+        # tar up the results
+        args = ['tar','cvfz',f'{label}.tar.gz','-C',tdir,label]
+        subprocess.run(args)
 
 class CleanUp:
     """
@@ -214,4 +230,4 @@ class CleanUp:
 
     def __exit__(self, type, value, traceback):
         print(f'removing temporary directory {self.tmpdir}')
-#        shutil.rmtree(self.tmpdir)
+        shutil.rmtree(self.tmpdir)
