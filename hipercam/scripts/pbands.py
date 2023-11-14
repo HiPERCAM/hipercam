5f668d6e471c862162422c12acd7c2df4e6e0dba
Stuart Littlefair <s.littlefair@shef.ac.uk>
2023-11-14 15:36:15 +0000

    fix pbands to work with more than 3 CCDs

--- hipercam/scripts/pbands.py
+++ hipercam/scripts/pbands.py
@@ -14,6 +14,7 @@ from trm.cline import Cline
 
 from astropy.timeseries import LombScargle
 
+
 def pbands(args=None):
     """``pbands log [device (dpi) width height ms line error] norm zero [plo phi increment]
     pgram (flo fhi over) title``
@@ -60,7 +61,7 @@ def pbands(args=None):
 
       plo : float [hidden]
          percentile to use to set the lower limit of the lightcurve plots
-         (e.g. 5). Takes account of error bars. 
+         (e.g. 5). Takes account of error bars.
 
       phi : float [hidden]
          percentile to use to set the upper limit of the lightcurve plot
@@ -101,12 +102,11 @@ def pbands(args=None):
     """
 
     command, args = cline.script_args(sys.argv)
-    if command.endswith('.py'):
+    if command.endswith(".py"):
         command = command[:-3]
 
     # get input section
     with Cline("HIPERCAM_ENV", ".hipercam", command, args) as cl:
-
         # register parameters
         cl.register("log", Cline.LOCAL, Cline.PROMPT)
         cl.register("device", Cline.LOCAL, Cline.HIDE)
@@ -137,146 +137,141 @@ def pbands(args=None):
         # load the reduce log
         hlog = hcam.hlog.Hlog.rascii(log)
 
-        cl.set_default("device","term")
+        cl.set_default("device", "term")
         device = cl.get_value("device", "plot device name", "term")
         if device.endswith(".png"):
             dpi = cl.get_value("dpi", "dots per inch", 240, 10)
 
         width = cl.get_value("width", "plot width (inches)", 0.0)
         height = cl.get_value("height", "plot height (inches)", 0.0)
-        ms = cl.get_value("ms", "marker size", 3., 0.)
+        ms = cl.get_value("ms", "marker size", 3.0, 0.0)
         line = cl.get_value("line", "connect points with a line", False)
         error = cl.get_value("error", "plot errorbars", True)
         aper1 = cl.get_value("aper1", "target aperture", "1")
-        aper2 = cl.get_value(
-            "aper2", "comparison aperture ('!' to ignore)", "2"
-        )
+        aper2 = cl.get_value("aper2", "comparison aperture ('!' to ignore)", "2")
 
         ccds = []
         for ccd in hlog.apnames:
-            if aper1 in hlog.apnames[ccd] and \
-               (aper2 == '!' or aper2 in hlog.apnames[ccd]):
+            if aper1 in hlog.apnames[ccd] and (
+                aper2 == "!" or aper2 in hlog.apnames[ccd]
+            ):
                 ccds.append(ccd)
 
         if len(ccds) == 0:
             raise hcam.HipercamError(
-                f'Failed to find any CCDs with apertures {aper1} and {aper2}'
+                f"Failed to find any CCDs with apertures {aper1} and {aper2}"
             )
         ccds.sort()
 
         norm = cl.get_value("norm", "normalise to median", True)
         zero = cl.get_value("zero", "set y-axis origin to zero", True)
-        plo = cl.get_value("plo", "percentile for plot lower limit", 0.0, 0., 100.)
-        phi = cl.get_value("phi", "percentile for plot upper limit", max(100.0,plo), plo, 100.)
-        increment = cl.get_value("increment", "plot range increment factor", 0.01, 0.)
+        plo = cl.get_value("plo", "percentile for plot lower limit", 0.0, 0.0, 100.0)
+        phi = cl.get_value(
+            "phi", "percentile for plot upper limit", max(100.0, plo), plo, 100.0
+        )
+        increment = cl.get_value("increment", "plot range increment factor", 0.01, 0.0)
         pgram = cl.get_value("pgram", "plot periodogram panels too", False)
         if pgram:
-            flo = cl.get_value("flo", "lower frequency limit (cycles/day)", 5.0, 0.)
+            flo = cl.get_value("flo", "lower frequency limit (cycles/day)", 5.0, 0.0)
             fhi = cl.get_value(
-                "fhi", "upper frequency limit (cycles/day)", max(flo,2000.), flo
+                "fhi", "upper frequency limit (cycles/day)", max(flo, 2000.0), flo
             )
-            over = cl.get_value("over", "oversampling factor", 5., 1.)
+            over = cl.get_value("over", "oversampling factor", 5.0, 1.0)
 
         if norm:
-            cl.set_default("title", f'{log} [median normalised]')
+            cl.set_default("title", f"{log} [median normalised]")
         else:
-            cl.set_default("title", f'{log}')
+            cl.set_default("title", f"{log}")
         title = cl.get_value("title", "plot title", "Plot Title")
 
         # Need to modify default according to numbers of CCDs
-        colours = cl.get_default("colours",('r','g','b'))
+        colours = cl.get_default("colours", ("r", "g", "b"))
         if len(colours) > len(ccds):
-            colours = colours[:len(ccds)]
-            cl.set_default("colours",colours)
+            colours = colours[: len(ccds)]
+            cl.set_default("colours", colours)
         elif len(colours) < len(ccds):
-            colours = tuple(colours) + (len(ccds)-len(colours))*('k',)
-            cl.set_default("colours",colours)
-        colours = cl.get_value(
-            "colours", "colours [strings, one per CCD]",
-            ('r','g','b')
-        )
+            colours = tuple(colours) + (len(ccds) - len(colours)) * ("k",)
+            cl.set_default("colours", colours)
+        colours = cl.get_value("colours", "colours [strings, one per CCD]", colours)
 
     if width > 0 and height > 0:
-        fig,axs = plt.subplots(
-            len(ccds),2 if pgram else 1,figsize=(width, height),
-            sharex='col', squeeze=False
+        fig, axs = plt.subplots(
+            len(ccds),
+            2 if pgram else 1,
+            figsize=(width, height),
+            sharex="col",
+            squeeze=False,
         )
     else:
-        fig,axs = plt.subplots(
-            len(ccds),2 if pgram else 1,
-            sharex='col',squeeze=False
+        fig, axs = plt.subplots(
+            len(ccds), 2 if pgram else 1, sharex="col", squeeze=False
         )
 
     # light curves
-    for ccd,ax,col in zip(ccds,axs[:,0],colours):
+    for ccd, ax, col in zip(ccds, axs[:, 0], colours):
         utils.style_mpl_axes(ax)
-        targ = hlog.tseries(ccd, aper1, 'counts')
+        targ = hlog.tseries(ccd, aper1, "counts")
         if aper2 != "!":
-            comp = hlog.tseries(ccd, aper2, 'counts')
+            comp = hlog.tseries(ccd, aper2, "counts")
             targ /= comp
         if norm:
             targ.normalise()
         if line:
-            targ.mplot(
-                ax, col, '-', bitmask=hcam.BAD_TIME|hcam.JUNK, erry=error
-            )
-        targ.mplot(
-            ax, col, bitmask=hcam.BAD_TIME|hcam.JUNK, ms=ms, erry=error
-        )
+            targ.mplot(ax, col, "-", bitmask=hcam.BAD_TIME | hcam.JUNK, erry=error)
+        targ.mplot(ax, col, bitmask=hcam.BAD_TIME | hcam.JUNK, ms=ms, erry=error)
 
         # plot limits
-        _d,y1,_d = targ.percentile(plo,hcam.BAD_TIME|hcam.JUNK)
-        _d,_d,y2 = targ.percentile(phi,hcam.BAD_TIME|hcam.JUNK)
+        _d, y1, _d = targ.percentile(plo, hcam.BAD_TIME | hcam.JUNK)
+        _d, _d, y2 = targ.percentile(phi, hcam.BAD_TIME | hcam.JUNK)
         if zero:
-            y1 = 0.
-            y2 *= 1+increment
+            y1 = 0.0
+            y2 *= 1 + increment
         else:
-            yrange = y2-y1
-            y1 -= increment*yrange
-            y2 += increment*yrange
+            yrange = y2 - y1
+            y1 -= increment * yrange
+            y2 += increment * yrange
 
-        ax.set_ylim(y1,y2)
+        ax.set_ylim(y1, y2)
 
         if aper2 == "!":
-            ax.set_ylabel('Targ')
+            ax.set_ylabel("Targ")
         else:
-            ax.set_ylabel('Targ/Comp')
+            ax.set_ylabel("Targ/Comp")
 
     if pgram:
         # periodograms
-        for ccd,ax,col in zip(ccds,axs[:,1],colours):
+        for ccd, ax, col in zip(ccds, axs[:, 1], colours):
             utils.style_mpl_axes(ax)
-            targ = hlog.tseries(ccd, aper1, 'counts')
+            targ = hlog.tseries(ccd, aper1, "counts")
             if aper2 != "!":
-                comp = hlog.tseries(ccd, aper2, 'counts')
+                comp = hlog.tseries(ccd, aper2, "counts")
                 targ /= comp
 
             # extract data
-            ts,_d,ys,yes = targ.get_data(hcam.BAD_TIME|hcam.JUNK)
-            ls = LombScargle(ts,ys,yes)
-            f,p = ls.autopower(
-                minimum_frequency=flo,maximum_frequency=fhi,
-                samples_per_peak=over
+            ts, _d, ys, yes = targ.get_data(hcam.BAD_TIME | hcam.JUNK)
+            ls = LombScargle(ts, ys, yes)
+            f, p = ls.autopower(
+                minimum_frequency=flo, maximum_frequency=fhi, samples_per_peak=over
             )
-            ax.plot(f,p,col)
-            ax.set_ylabel('Power')
-            y1,y2 = ax.set_ylim()
-            ax.set_ylim(0,y2)
-    
-        axs[0,0].set_title(f'{title} [light curves]')
-        axs[0,1].set_title(f'{title} [periodograms]')
-        axs[-1,0].set_xlabel('Time [MJD]')
-        axs[-1,1].set_xlabel('Frequency [cycles/day]')
-        
+            ax.plot(f, p, col)
+            ax.set_ylabel("Power")
+            y1, y2 = ax.set_ylim()
+            ax.set_ylim(0, y2)
+
+        axs[0, 0].set_title(f"{title} [light curves]")
+        axs[0, 1].set_title(f"{title} [periodograms]")
+        axs[-1, 0].set_xlabel("Time [MJD]")
+        axs[-1, 1].set_xlabel("Frequency [cycles/day]")
+
     else:
-        axs[0,0].set_title(f'{title}')
-        axs[-1,0].set_xlabel('Time [MJD]')
+        axs[0, 0].set_title(f"{title}")
+        axs[-1, 0].set_xlabel("Time [MJD]")
 
     plt.tight_layout()
     if device == "term":
         plt.show()
     else:
         if device.endswith(".png"):
-            plt.savefig(device,dpi=dpi)
+            plt.savefig(device, dpi=dpi)
         else:
             plt.savefig(device)
