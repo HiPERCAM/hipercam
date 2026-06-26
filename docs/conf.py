5027f47a3e0e56c131cb45c57c8463b6f07c8b6e
Stuart Littlefair <s.littlefair@shef.ac.uk>
2026-06-26 15:33:27 +0100

    turn on autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -32,10 +32,10 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.imgmath',
-    'numpydoc',
-    'autoapi.extension',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.imgmath",
+    "numpydoc",
+    "autoapi.extension",
 ]
 
 # this needed for numpydoc I think
@@ -45,25 +45,25 @@ numpydoc_show_inherited_class_members = False
 # Autoapi settings
 # Note we don't actually create the auto-generated API pages for now,
 # instead we write our own pages (e.g. api/hipercam-ccd.rst, scripts in commands.rst)
-autoapi_dirs = ['../hipercam']
-autoapi_generate_api_docs = False
+autoapi_dirs = ["../hipercam"]
+autoapi_generate_api_docs = True
 
 # number figures
 numfig = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-source_suffix = {'.rst': 'restructuredtext'}
+source_suffix = {".rst": "restructuredtext"}
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'HiPERCAM pipeline'
-copyright = '2017, T.R. Marsh'
-author = 'T.R. Marsh'
+project = "HiPERCAM pipeline"
+copyright = "2017, T.R. Marsh"
+author = "T.R. Marsh"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -71,13 +71,15 @@ author = 'T.R. Marsh'
 try:
     # Try to get version from installed package first
     from importlib.metadata import version as _version
-    release = _version('hipercam')
+
+    release = _version("hipercam")
     print(f"[Sphinx] Using installed package version: {release}")
 except ImportError:
     # Fallback to setuptools_scm if package not installed
     try:
         from setuptools_scm import get_version
-        release = get_version(root='..', relative_to=__file__)
+
+        release = get_version(root="..", relative_to=__file__)
         print(f"[Sphinx] Using setuptools_scm version: {release}")
     except (ImportError, LookupError) as e:
         # Final fallback to a default version
@@ -94,10 +96,10 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -107,47 +109,45 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-#html_theme = 'classic'
-html_theme = 'bootstrap-astropy'
+# html_theme = 'alabaster'
+# html_theme = 'classic'
+html_theme = "bootstrap-astropy"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'logotext2': '',  # orange, light
-    'logotext3': ':docs',   # white,  light
-    'astropy_project_menubar': False
-    }
+    "logotext2": "",  # orange, light
+    "logotext3": ":docs",  # white,  light
+    "astropy_project_menubar": False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-#html_sidebars = {
+# html_sidebars = {
 #    '**': [
 #        'relations.html',  # needs 'show_related': True theme option to display
 #        'searchbox.html',
 #    ]
-#}
+# }
 
 html_sidebars = {
-    '**': [
-        'globaltoc.html', 'relations.html',
-        'sourcelink.html', 'searchbox.html'],
+    "**": ["globaltoc.html", "relations.html", "sourcelink.html", "searchbox.html"],
 }
 
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'theHiPERCAMpipelinedoc'
+htmlhelp_basename = "theHiPERCAMpipelinedoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -156,15 +156,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -174,8 +171,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'theHiPERCAMpipeline.tex', 'the HiPERCAM pipeline Documentation',
-     'T.R. Marsh', 'manual'),
+    (
+        master_doc,
+        "theHiPERCAMpipeline.tex",
+        "the HiPERCAM pipeline Documentation",
+        "T.R. Marsh",
+        "manual",
+    ),
 ]
 
 
@@ -184,8 +186,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'thehipercampipeline', 'the HiPERCAM pipeline Documentation',
-     [author], 1)
+    (
+        master_doc,
+        "thehipercampipeline",
+        "the HiPERCAM pipeline Documentation",
+        [author],
+        1,
+    )
 ]
 
 
@@ -195,7 +202,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'theHiPERCAMpipeline', 'the HiPERCAM pipeline Documentation',
-     author, 'theHiPERCAMpipeline', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "theHiPERCAMpipeline",
+        "the HiPERCAM pipeline Documentation",
+        author,
+        "theHiPERCAMpipeline",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
